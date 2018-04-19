d7ac2bc600cc8fc981996cefc0182883e28bf2fb
MatsudaYoshio <dark.unicorn1213@gmail.com>
2018-04-19 22:39:26 +0900

    lcs 追加と修正

--- Algorithms/lcs.c
+++ Algorithms/lcs.c
@@ -1,21 +1,20 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <cstdio>
-
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
 enum Direction {DIAGONAL, VERTICAL, HORIZONTAL};
 
-void lcs(const string& x, const string& y){
-  auto m {x.size()};
-  auto n {y.size()};
-
-  int dp[m+1][n+1], t[m+1][n+1];
-  fill(dp[0], dp[m+1], 0);
+void lcs(const char* x, const char* y){
+  int i, j, k;
   
-  for(int i = 0; i < m; ++i){
-    for(int j = 0; j < n; ++j){
+  int m = strlen(x);
+  int n = strlen(y);
+  
+  int dp[m+1][n+1], t[m+1][n+1];
+  for(i = 0; i <= m; ++i) dp[i][0] = 0;
+  for(i = 1; i <= n; ++i) dp[0][i] = 0;
+  
+  for(i = 0; i < m; ++i){
+    for(j = 0; j < n; ++j){
       if(x[i] == y[j]){
         dp[i+1][j+1] = dp[i][j]+1;
         t[i+1][j+1] = DIAGONAL;
@@ -28,9 +27,15 @@ void lcs(const string& x, const string& y){
       }
     }
   }
-  
-  string lcs(dp[m][n], '\0');
-  for(int i = m, j = n, k = dp[m][n]; k > 0;){
+
+  int lcs_size = dp[m][n];
+  char lcs[lcs_size+1];
+  lcs[lcs_size] = '\0';
+
+  i = m;
+  j = n;
+  k = lcs_size;
+  while(i != 0 && j != 0){
     switch(t[i][j]){
     case DIAGONAL:
       lcs[--k] = x[i-1];
@@ -45,11 +50,11 @@ void lcs(const string& x, const string& y){
       break;
     }
   }
-
-  printf("%d(\"%s\")\n", dp[m][n], lcs.c_str());
+  
+  printf("%d(\"%s\")\n", lcs_size, lcs);
 } 
 
 int main()
 {
-  lcs("abcbdab", "bdcaba");
+  lcs("aabacbab", "cbcacabca");
 }

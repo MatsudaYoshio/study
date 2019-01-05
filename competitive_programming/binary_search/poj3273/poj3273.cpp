202e266ade01fe054a2b3d1ef600a0dc20daab7e
MatsudaYoshio <dark.unicorn1213@gmail.com>
2019-01-05 12:53:34 +0900

    poj3273 追加

---
+++ competitive_programming/binary_search/poj3273/poj3273.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+vector<int> E;
+
+bool C(int e)
+{
+  int sum;
+  int m = M;
+  for(int i = 0; i < N;){
+
+    sum = 0;
+    while(1){
+      sum += E[i++];
+      if(sum > e){
+        --i;
+        --m;
+        break;
+      }else if(i == N){
+        --m;
+        break;
+      }
+    }
+
+    if(m < 0) return false;
+    
+  }
+  return true;
+}
+
+int main()
+{
+  cin >> N >> M;
+  E.resize(N);
+
+  for(int i = 0; i < N; ++i) cin >> E[i];
+
+  int lb = 0, ub = 1000000000, mid;
+
+  while(ub - lb > 1){
+    mid = (lb + ub)/2;
+    cout << mid << endl;
+    if(C(mid)) ub = mid;
+    else lb = mid;
+  }
+
+  cout << ub << endl;
+}

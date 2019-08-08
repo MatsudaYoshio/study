8e191f4c723525a07795c648f63a6090d1933bea
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2019-08-08 22:21:30 +0900

    正規性の検定　修正

--- statistical_analysis/wilcoxon_signed_rank_test.R
+++ statistical_analysis/wilcoxon_signed_rank_test.R
@@ -17,13 +17,15 @@ item.name <- str_sub(colnames(all.data[2:(2+item.num-1)]), start = 1, end = -4)
 data1 <- all.data[2:(2+item.num-1)] 
 data2 <- all.data[(2+item.num):(2*item.num+1)]
 
-# 正規性の検定（シャピロ・ウィルク検定）を行い、帰無仮説（標本分布が正規分布に従う）が棄却されることを確認する
-all.vector <- append(as.vector(t(data1)), as.vector(t(data2)))
-shapiro.test(x=all.vector)
-
-# 各項目でウィルコクソンの符号順位検定を行う
 for (i in 1:item.num){
 	cat(sprintf('-------------- %s --------------\n', item.name[i]))
+
+	# 各項目の全部のデータを使って正規性の検定（シャピロ・ウィルク検定）を行い、帰無仮説（標本分布が正規分布に従う）が棄却されることを確認する
+	all.vector <- append(as.vector(t(data1[i])), as.vector(t(data2[i])))
+	print(shapiro.test(x=all.vector))
+	
+	# 各項目でウィルコクソンの符号順位検定を行う	
 	print(wilcox.exact(x=t(data1[i]), y=t(data2[i]), paired=T))
+	
 	cat(sprintf('--------------------------------------------------\n', item.name[i]))
 }

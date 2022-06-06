fb2bc27a8ec845f11585be7bfde61ebacd3c602d
Matsuda Yoshio <dark.unicorn1213@gmail.com>
2022-06-06 17:20:22 +0900

    Fix: simplify if logic

--- ProgrammingForThePuzzled/chapter12/circular_toh.py
+++ ProgrammingForThePuzzled/chapter12/circular_toh.py
@@ -1,9 +1,6 @@
 def cw(num_rings, start_peg, end_peg):
     num_moves = 0
-    if num_rings == 1:
-        print('Move ring', num_rings, 'from peg', start_peg, 'to peg', end_peg)
-        num_moves += 1
-    else:
+    if num_rings > 0:
         mid_peg = 6 - start_peg - end_peg
 
         num_moves += ccw(num_rings-1, start_peg, mid_peg)
@@ -18,13 +15,9 @@ def cw(num_rings, start_peg, end_peg):
 
 def ccw(num_rings, start_peg, end_peg):
     num_moves = 0
-    mid_peg = 6 - start_peg - end_peg
+    if num_rings > 0:
+        mid_peg = 6 - start_peg - end_peg
 
-    if num_rings == 1:
-        print('Move ring', num_rings, 'from peg', start_peg, 'to peg', mid_peg)
-        print('Move ring', num_rings, 'from peg', mid_peg, 'to peg', end_peg)
-        num_moves += 2
-    elif num_rings > 0:
         num_moves += cw(num_rings-1, start_peg, mid_peg)
         num_moves += cw(num_rings-1, mid_peg, end_peg)
 
